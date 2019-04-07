847f17a3fd86dbe5d9d55269a16fc4b31befe952
Josh Muncke <joshxthomas@gmail.com>
2019-04-07 00:57:13 -0700

    Folder functions

--- R/folders.R
+++ R/folders.R
@@ -11,14 +11,17 @@ create_project_folders <- function(project_name, where = config::get("bulldrive"
   # Convert lower cases, strip punctuation
   project_name <- stringr::str_to_lower(project_name)
 
-  egnyter::create_folder(where, project_name)
-
   project_folder = paste(where, project_name, sep = "/")
 
-  egnyter::create_folder(project_folder, "1-input")
-  egnyter::create_folder(project_folder, "2-data")
-  egnyter::create_folder(project_folder, "3-analysis")
-  egnyter::create_folder(project_folder, "4-output")
-  egnyter::create_folder(project_folder, "5-presentations")
-  egnyter::create_folder(project_folder, "9-admin")
+  user_confirmation <- menu(c("Yes", "No"), title = "Are you sure you want to create the project folders under [", project_folder ,"]")
+
+  if (user_confirmation == 1) {
+    egnyter::create_folder(where, project_name)
+    egnyter::create_folder(project_folder, "1-input")
+    egnyter::create_folder(project_folder, "2-data")
+    egnyter::create_folder(project_folder, "3-analysis")
+    egnyter::create_folder(project_folder, "4-output")
+    egnyter::create_folder(project_folder, "5-presentations")
+    egnyter::create_folder(project_folder, "9-admin")
+  }
 }
