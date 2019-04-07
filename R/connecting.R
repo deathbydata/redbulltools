ff29255fdcecae140377ceea0a1131c09bbe11c3
Josh Muncke <joshxthomas@gmail.com>
2019-04-06 23:05:09 -0700

    Connect to DAP

---
+++ R/connecting.R
@@ -0,0 +1,11 @@
+#' Quick-connect to the DAP system
+#'
+#' This function is a wrapper around the DBI::dbConnect
+#' function and presumes you have appropriate config
+#' and environment variables set up.
+#'
+#'
+#' @export
+connect_to_dap <- function() {
+  print("Hello, world!")
+}
