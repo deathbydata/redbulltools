14853cb5fd9951007b6c440d21f91051d414452a
Josh Muncke <josh.muncke@redbull.com>
2019-07-13 00:09:27 +0200

    Improve code for create object

--- R/database.R
+++ R/database.R
@@ -5,16 +5,32 @@
 #' the top-level folder name.
 #'
 #' @param x dbplyr pipe / remote tbl object
-#' @param name name for new remote object
+#' @param name name for new remote object (including schema)
 #' @param dest remote data source connection
 #' @param type specify whether the object is a view (default or table)
 #'
 #' @export
 #' @importFrom magrittr %>%
 create_database_object <- function(x, name, dest, type = c("view", "table")) {
-  # Get the raw value
+
+  # Get the raw query string for the query object
   raw_query <- dbplyr::remote_query(query)
+
+  # Strip the "<SQL>" header text that remote_query adds
   strip_header <- stringr::str_replace_all(raw_query, "<SQL>", "")
-  view_query <- paste0("CREATE VIEW ", view_name, " AS (", strip_header, ");")
-  DBI::dbGetQuery(connection, view_query)
+
+  # What are we creating?
+  if(type == "view") {
+    create_text <- "CREATE VIEW "
+  }
+  else {
+    create_text <- "CREATE TABLE "
+  }
+
+  # SQL code to create the new object
+  object_create_sql <- paste0(create_text, name," AS (", strip_header ,");")
+
+  # Execute the create code
+  # Need to test if already exists??
+  DBI::dbGetQuery(dest, object_create_sql)
 }
